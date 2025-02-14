b36fb75601b8063ef743a009db7b32f8ce43cc44
sandovaldavid <sandovaldavid2201@gmail.com>
2025-02-14 00:47:03 -0500

    ♻️ Refactor page and layout components to use function declarations

--- app/dashboard/channels/[id]/page.tsx
+++ app/dashboard/channels/[id]/page.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-
-export const page = () => {
+function page() {
     return <div>page</div>;
-};
+}
+
+export default page;
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
-export const layout = () => {
+function layout() {
     return <div>layout</div>;
-};
+}
+
+export default layout;
