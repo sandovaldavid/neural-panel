b4448d813b87189bc966a49281df29f8ba155d2f
sandovaldavid <sandovaldavid2201@gmail.com>
2025-02-14 00:14:14 -0500

    ♻️  Refactor styles: move globals.css to app/styles and update Tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,26 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            discord: {
+                primary: '#5865F2',
+                success: '#57F287',
+                danger: '#ED4245',
+                dark: '#36393F',
+                darker: '#202225',
+                light: '#FFFFFF',
+                accent: '#99AAB5',
+                neon: '#00fff5',
+                purple: '#9B6DFF',
+                darkBlue: '#1a1c2c',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 } satisfies Config;
